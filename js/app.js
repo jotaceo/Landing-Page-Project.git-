7b07491211957bcec11416d7d209c7c3b8e3731c
JC <none>
2022-06-16 20:15:06 -0400

    Fixed H1 responsivness, hopefully fixed sections marked highlight on portrait view, changes variables with var to let, fixed some white space between brackets.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@
 let sections = document.querySelectorAll('section');
 // Used to iterate in the setVieportActive function
 let i = 0;
-var sectionId = '';
+let sectionId = '';
 
 /**
  * End Global Variables
@@ -43,73 +43,68 @@ var sectionId = '';
 
 // build fourth section
 function addNewSection() {
-  // set a variable for the main HTML element
-  const main = document.querySelector('main');
-  // Set a variable to check if we can create a section has last child.
-  // It was too late when I noticed this wasn't necessary, but I already spent time doing it.
-  // It could be useful, if an element other than a section is added as last child in the main
-  // element. This will add the new section after the last section already in the main container
-  // and before that other element acting as last child in the main, always that after that element there is not a section.
-  let canWeCreateASection = false;
+	// set a variable for the main HTML element
+	const main = document.querySelector('main');
+	// Set a variable to check if we can create a section has last child.
+	// It was too late when I noticed this wasn't necessary, but I already spent time doing it.
+	// It could be useful, if an element other than a section is added as last child in the main
+	// element. This will add the new section after the last section already in the main container
+	// and before that other element acting as last child in the main, always that after that element there is not a section.
+	let canWeCreateASection = false;
 
-  // checks if main have child nodes and set to true the variable canWeCreateASection if so
-  if (main.hasChildNodes) {
-    // iterates child nodes of main
-    for (var mainNode of main.children) {
-      // get sure we have a section in the main element
-      if (mainNode.nodeName == 'SECTION') {
-        canWeCreateASection = true;
-      }
-    }
-  }
+	// checks if main have child nodes and set to true the variable canWeCreateASection if so
+	if (main.hasChildNodes) {
+		// iterates child nodes of main
+		for (let mainNode of main.children) {
+			// get sure we have a section in the main element
+			if (mainNode.nodeName == 'SECTION') {
+				canWeCreateASection = true;
+			}
+		}
+	}
 
-  // If we have 'green light' then we proceed to create and append the new section
-  if (canWeCreateASection == true) {
-    // creates a new section
-    let newSection = document.createElement('section');
-    // It get sure we are adding the section after the last section already in the main container
-    sections[sections.length - 1].parentNode.insertBefore(
-      newSection,
-      sections[sections.length - 1].nextSibling
-    );
-    // count again the sections to help capture the last child of them
-    sections = document.querySelectorAll('section');
-    // sets the string for id value
-    let idValue = 'section' + sections.length;
-    // sets the string for data type value
-    let dataType = 'Section ' + sections.length;
-    // adds id and data- properties to the section
-    sections[sections.length - 1].setAttribute('id', idValue);
-    sections[sections.length - 1].setAttribute('data-nav', dataType);
-    // creates a div
-    let newDiv = document.createElement('div');
-    // adds a div to the section
-    sections[sections.length - 1].appendChild(newDiv);
-    // selects a div to prepare it to add classes on it
-    let theDiv = sections[sections.length - 1].querySelector('div');
-    // sets class to the div
-    theDiv.setAttribute('class', 'landing__container');
-    // creates a h2
-    let newH2 = document.createElement('h2');
-    // adds a H2 element to the div
-    theDiv.appendChild(newH2);
-    // We are going to use dataType to set the textContent
-    newH2.textContent = dataType;
-    // creates a first paragraph
-    const newP1 = document.createElement('p');
-    // set a textContent
-    newP1.textContent =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
-    // add a new paragraph
-    theDiv.appendChild(newP1);
-    // creates a second paragraph
-    const newP2 = document.createElement('p');
-    // set a textContent
-    newP2.textContent =
-      'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
-    // add a new paragraph
-    theDiv.appendChild(newP2);
-  }
+	// If we have 'green light' then we proceed to create and append the new section
+	if (canWeCreateASection == true) {
+		// creates a new section
+		let newSection = document.createElement('section');
+		// It get sure we are adding the section after the last section already in the main container
+		sections[sections.length - 1].parentNode.insertBefore(newSection,	sections[sections.length - 1].nextSibling);
+		// count again the sections to help capture the last child of them
+		sections = document.querySelectorAll('section');
+		// sets the string for id value
+		let idValue = 'section' + sections.length;
+		// sets the string for data type value
+		let dataType = 'Section ' + sections.length;
+		// adds id and data- properties to the section
+		sections[sections.length - 1].setAttribute('id', idValue);
+		sections[sections.length - 1].setAttribute('data-nav', dataType);
+		// creates a div
+		let newDiv = document.createElement('div');
+		// adds a div to the section
+		sections[sections.length - 1].appendChild(newDiv);
+		// selects a div to prepare it to add classes on it
+		let theDiv = sections[sections.length - 1].querySelector('div');
+		// sets class to the div
+		theDiv.setAttribute('class', 'landing__container');
+		// creates a h2
+		let newH2 = document.createElement('h2');
+		// adds a H2 element to the div
+		theDiv.appendChild(newH2);
+		// We are going to use dataType to set the textContent
+		newH2.textContent = dataType;
+		// creates a first paragraph
+		const newP1 = document.createElement('p');
+		// set a textContent
+		newP1.textContent =	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
+		// add a new paragraph
+		theDiv.appendChild(newP1);
+		// creates a second paragraph
+		const newP2 = document.createElement('p');
+		// set a textContent
+		newP2.textContent =	'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
+		// add a new paragraph
+		theDiv.appendChild(newP2);
+	}
 }
 addNewSection();
 
@@ -119,78 +114,83 @@ const menuUl = document.querySelector('#navbar__list');
 
 // Function to create links in the menu
 function createLink() {
-  // testing loop
-  for (const section of sections) {
-    // creates links in the menu
-    const menuLi = document.createElement('li');
-    // add class to the li
-    menuLi.classList.add('menu__link');
-    // sets the string for data type value and textContent
-    let sectionTitle = section.getAttribute('data-nav');
-    // add attribute data-nav
-    menuLi.setAttribute('data-nav', sectionTitle);
-    // add textContent
-    menuLi.innerHTML = `${sectionTitle}`;
-    // Scroll to anchor ID using scrollTO event
-    // Set element by id
-    sectionId = section.getAttribute('id');
-    let scrollToId = document.getElementById(sectionId);
+	// testing loop
+	for (const section of sections) {
+		// creates links in the menu
+		const menuLi = document.createElement('li');
+		// add class to the li
+		menuLi.classList.add('menu__link');
+		// sets the string for data type value and textContent
+		let sectionTitle = section.getAttribute('data-nav');
+		// add attribute data-nav
+		menuLi.setAttribute('data-nav', sectionTitle);
+		// add textContent
+		menuLi.innerHTML = `${sectionTitle}`;
+		// Scroll to anchor ID using scrollTO event
+		// Set element by id
+		sectionId = section.getAttribute('id');
+		let scrollToId = document.getElementById(sectionId);
 
-    // Scroll to section on link click
-    goToSection = function() {
-      scrollToId.scrollIntoView({ behavior: 'smooth' });
-      for (let section of sections) {
-        section.classList.remove('your-active-class');
-      }
-      // Set sections as active
-      scrollToId.classList.add('your-active-class');
-    }
-    // if a li is clicked, the page will scroll to a section
-    menuLi.addEventListener('click', goToSection);
-    // add li on the unordered list using Append
-    menuUl.append(menuLi);
-  }
+		// Scroll to section on link click
+		goToSection = function () {
+			scrollToId.scrollIntoView({behavior: 'smooth'});
+			for (let section of sections) {
+				section.classList.remove('your-active-class');
+			}
+			// Set sections as active
+			scrollToId.classList.add('your-active-class');
+		};
+		// if a li is clicked, the page will scroll to a section
+		menuLi.addEventListener('click', goToSection);
+		// add li on the unordered list using Append
+		menuUl.append(menuLi);
+	}
 }
 createLink();
 
 // Add class 'active' to section when near top of viewport
 for (let section of sections) {
-  sections[i].classList.remove('your-active-class');
+	sections[i].classList.remove('your-active-class');
 }
 window.onscroll = setViewportActivate;
 function setViewportActivate() {
-  console.clear();
-  for (const section of sections) {
-    console.log(i);
-    let sectionDomRect = sections[i].getBoundingClientRect();
-    console.log(sectionDomRect.top);
-    console.log(sectionDomRect.bottom);
-    // Removes sections as active and navigation as highlighted
-    if (sectionDomRect.top > 400) {
-      console.log('top out of view');
-      sections[i].classList.remove('your-active-class');
-      let navLink = document.querySelectorAll('li');
-      navLink[i].classList.remove('nav-highlighted');
-    }
-    // Set sections as active and navigation gets highlighted
-    if (sectionDomRect.top < 525) {
-      console.log(sections[i]);
-      sections[i].classList.add('your-active-class');
-      let navLink = document.querySelectorAll('li');
-      navLink[i].classList.add('nav-highlighted');
-    }
-    // Removes sections as active and navigation as highlighted
-    if (sectionDomRect.bottom < 525) {
-      console.log('top out of view');
-      sections[i].classList.remove('your-active-class');
-      let navLink = document.querySelectorAll('li');
-      navLink[i].classList.remove('nav-highlighted');
-    }
-    i++;
-    if (i > 3) {
-      i = 0;
-    }
-  }
+	console.clear();
+	for (const section of sections) {
+		console.log(i);
+		let sectionDomRect = sections[i].getBoundingClientRect();
+		console.log(sectionDomRect.top);
+		console.log(sectionDomRect.bottom);
+
+		// Removes sections as active and navigation as highlighted
+		if (sectionDomRect.top > 130) {
+			console.log('top out of view');
+			sections[i].classList.remove('your-active-class');
+			let navLink = document.querySelectorAll('li');
+			navLink[i].classList.remove('nav-highlighted');
+		}
+
+		// Set sections as active and navigation gets highlighted
+		if (sectionDomRect.top < 129) {
+			console.log(sections[i]);
+			sections[i].classList.add('your-active-class');
+			let navLink = document.querySelectorAll('li');
+			navLink[i].classList.add('nav-highlighted');
+		}
+
+		// Removes sections as active and navigation as highlighted
+		if (sectionDomRect.bottom < 128) {
+			console.log('top out of view');
+			sections[i].classList.remove('your-active-class');
+			let navLink = document.querySelectorAll('li');
+			navLink[i].classList.remove('nav-highlighted');
+		}
+    
+    // This will help that scrolling don't try to add or remove the your-active-class to an element tha doesn't exist.
+		i++;
+		if (i > sections.length - 1) {
+			i = 0;
+		}
+	}
 }
 setViewportActivate();
 
